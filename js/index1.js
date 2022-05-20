f835f7e16f5ec0a4b3ad8ee66bbec333865b6a60
Aizhamal <aizhamal.dzhambulatova.kk@gmail.com>
2022-05-20 17:45:51 +0600

    add js files

---
+++ js/index1.js
@@ -0,0 +1,4 @@
+//1. Простое сообщение: сохраните текстовое сообщение в переменной и выведите его в браузере
+let message = "I'm starting to study JS!";
+alert(message);
+//document.write(message);
---
+++ js/index3.js
@@ -0,0 +1,6 @@
+//3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let userName = prompt("Please type your name");
+let userMail = prompt("Please type your mail");
+
+alert("Hello " + userName + ", " + " your mail address is: " + userMail);
